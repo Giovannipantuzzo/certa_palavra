0f6605a2dbff62ee19f089dc8dc3b074de065053
Matheus <matheusmiranda98@live.com>
2022-06-15 00:41:09 -0300

    Fix babel rc, Home

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,52 +1,51 @@
 import React from 'react';
-import './MenuLateral.css';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
+// import './MenuLateral.css';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import Button from '@mui/material/Button';
+// import ButtonGroup from '@mui/material/ButtonGroup';
+// import Box from '@mui/material/Box';
 
-const buttons = [
-  { buttonName: 'Dashboard' },
-  { buttonName: 'Informações' },
-];
+// const buttons = [
+//   { buttonName: 'Dashboard' },
+//   { buttonName: 'Informações' },
+// ];
 
-const theme = createTheme({
-  palette: {
-    secondary: {
-      main: '#264A6F',
-    },
-  },
-});
+// const theme = createTheme({
+//   palette: {
+//     secondary: {
+//       main: '#264A6F',
+//     },
+//   },
+// });
 
-function MenuLateral({ setSelectedButton, selectedButton }) {
-  const defineBackgroundColor = (buttonType) => (selectedButton === buttonType
-    ? 'menuSideClickButton'
-    : 'menuSideGrupButton');
+export default function Home({ setSelectedButton, selectedButton }) {
+  // const defineBackgroundColor = (buttonType) => (selectedButton === buttonType
+  //   ? 'menuSideClickButton'
+  //   : 'menuSideGrupButton');
 
   return (
-    <Box>
-      <div className="menuSidepage">
-        <div className="menuSideContainer">
-          <ThemeProvider theme={theme}>
-            <ButtonGroup
-              className="menuSideAll"
-              size="large"
-              orientation="vertical"
-              aria-label="vertical contained button group"
-              variant="text"
-              color="secondary"
-            >
+    // <Box>
+    //   <div className="menuSidepage">
+    //     <div className="menuSideContainer">
+    //       <ThemeProvider theme={theme}>
+    //         <ButtonGroup
+    //           className="menuSideAll"
+    //           size="large"
+    //           orientation="vertical"
+    //           aria-label="vertical contained button group"
+    //           variant="text"
+    //           color="secondary"
+    //         >
 
-              {buttons.map((button) => (
-                <Button key={`${button.buttonName}`} className={defineBackgroundColor(button.buttonName)} onClick={() => setSelectedButton(button.buttonName)}>{button.buttonName}</Button>
-              ))}
+    //           {buttons.map((button) => (
+    //             <Button key={`${button.buttonName}`} className={defineBackgroundColor(button.buttonName)} onClick={() => setSelectedButton(button.buttonName)}>{button.buttonName}</Button>
+    //           ))}
 
-            </ButtonGroup>
-          </ThemeProvider>
-        </div>
-      </div>
-    </Box>
+    //         </ButtonGroup>
+    //       </ThemeProvider>
+    //     </div>
+    //   </div>
+    // </Box>
+    <>biro do biro biro</>
   );
 }
-
-export default MenuLateral;
