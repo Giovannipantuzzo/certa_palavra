64ccc9f2bde672ffb72b0f11bbd3f358d8b1afa9
Matheus <matheusmiranda98@live.com>
2022-06-26 21:57:28 -0300

    functional editor

--- pages/Editor/index.js
+++ pages/Editor/index.js
@@ -8,13 +8,14 @@ const DynamicComponentWithNoSSR = dynamic(
 );
 
 export default function Editor() {
-  const [url, setUrl] = useState("img/sampleImage.jpg");
+  const [url, setUrl] = useState("https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/526px-Wikipedia-logo-v2.svg.png");
   return (
     <div
       style={{
         width: "100vw",
-        height: "100vh",
+        height: "125vh",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         marginBottom: "5%",
--- src/components/ImageEditor/imageEditor.js
+++ src/components/ImageEditor/imageEditor.js
@@ -1,135 +1,291 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import reactCSS from "reactcss";
 import ImageEditor from "@toast-ui/react-image-editor";
+import { storage } from "./firebaseStorage";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { dataURLtoFile } from "./dataUrlToFile";
+import { toast } from "react-toastify";
+import { theme } from "./theme";
+import LinearProgress from "@mui/material/LinearProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import {
+  Submit,
+  LinearProgressContainer,
+  ButtonsMenuContainer,
+  ColorSelectedText,
+  ColorPickerContainer,
+} from "../../../styles/imageEditorStyles";
 import "tui-image-editor/dist/tui-image-editor.css";
+import "tui-color-picker/dist/tui-color-picker.css";
+import { SketchPicker } from "react-color";
+import $ from "jquery";
 
-const theme = {
-  "common.bi.image":
-    "https://logodownload.org/wp-content/uploads/2017/04/instagram-logo.png",
-  "common.bisize.width": "50px",
-  "common.bisize.height": "50px",
-  "common.backgroundImage": "none",
-  "common.backgroundColor": "#fff",
-  "common.border": "1px solid #c1c1c1",
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
 
-  // header
-  "header.backgroundImage": "none",
-  "header.backgroundColor": "transparent",
-  "header.border": "0px",
+toast.configure();
 
-  // load button
-  "loadButton.backgroundColor": "#fff",
-  "loadButton.border": "1px solid #ddd",
-  "loadButton.color": "#222",
-  "loadButton.fontFamily": "NotoSans, sans-serif",
-  "loadButton.fontSize": "12px",
-
-  // download button
-  "downloadButton.backgroundColor": "#fdba3b",
-  "downloadButton.border": "1px solid #fdba3b",
-  "downloadButton.color": "#fff",
-  "downloadButton.fontFamily": "NotoSans, sans-serif",
-  "downloadButton.fontSize": "12px",
-
-  // icons default
-  "menu.normalIcon.color": "#8a8a8a",
-  "menu.activeIcon.color": "#555555",
-  "menu.disabledIcon.color": "#434343",
-  "menu.hoverIcon.color": "#e9e9e9",
-  "submenu.normalIcon.color": "#8a8a8a",
-  "submenu.activeIcon.color": "#e9e9e9",
-
-  "menu.iconSize.width": "24px",
-  "menu.iconSize.height": "24px",
-  "submenu.iconSize.width": "32px",
-  "submenu.iconSize.height": "32px",
-
-  // submenu primary color
-  "submenu.backgroundColor": "#1e1e1e",
-  "submenu.partition.color": "#858585",
-
-  // submenu labels
-  "submenu.normalLabel.color": "#858585",
-  "submenu.normalLabel.fontWeight": "lighter",
-  "submenu.activeLabel.color": "#fff",
-  "submenu.activeLabel.fontWeight": "lighter",
-
-  // checkbox style
-  "checkbox.border": "1px solid #ccc",
-  "checkbox.backgroundColor": "#fff",
-
-  // rango style
-  "range.pointer.color": "#fff",
-  "range.bar.color": "#666",
-  "range.subbar.color": "#d1d1d1",
-
-  "range.disabledPointer.color": "#414141",
-  "range.disabledBar.color": "#282828",
-  "range.disabledSubbar.color": "#414141",
-
-  "range.value.color": "#fff",
-  "range.value.fontWeight": "lighter",
-  "range.value.fontSize": "11px",
-  "range.value.border": "1px solid #353535",
-  "range.value.backgroundColor": "#151515",
-  "range.title.color": "#fff",
-  "range.title.fontWeight": "lighter",
-
-  // colorpicker style
-  "colorpicker.button.border": "1px solid #1e1e1e",
-  "colorpicker.title.color": "#fff",
-};
-
-const CustomImageEditor = ({ url }) => {
+function CustomImageEditor({ url }) {
   const canvasRef = useRef();
-  console.log(canvasRef)
+
+  const [imgUrl, setImgUrl] = useState(url);
+  const [colorWidth, setColorWidth] = useState(10);
+  const [progresspercent, setProgresspercent] = useState(0);
+  const [presetColors, setPresetColors] = useState([
+    "#ffff00",
+    "#993399",
+    "#008000",
+    "#ffa500",
+    "#ffcbdb",
+  ]);
+  const [selectedColor, setSelectedColor] = useState({
+    displayColorPicker: false,
+    start: false,
+    color: {
+      r: "255",
+      g: "255",
+      b: "0",
+      a: "1",
+    },
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let imageEditor = canvasRef.current.getInstance();
+
+    var file = dataURLtoFile(
+      imageEditor.toDataURL(),
+      `teste.${imageEditor.toDataURL().match(/[^:/]\w+(?=;|,)/)[0]}`
+    );
+    if (!file) return;
+    const storageRef = ref(storage, `files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgresspercent(progress);
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgUrl(downloadURL);
+          toast.success("Editado com sucesso", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+          });
+        });
+      }
+    );
+  }
+
+  function LoadImage() {
+    if (!imgUrl) return;
+    let imageEditor = canvasRef.current.getInstance();
+    imageEditor.loadImageFromURL = (function () {
+      var cached_function = imageEditor.loadImageFromURL;
+      function waitUntilImageEditorIsUnlocked(imageEditor) {
+        return new Promise((resolve, reject) => {
+          const interval = setInterval(() => {
+            if (!imageEditor._invoker._isLocked) {
+              clearInterval(interval);
+              resolve();
+            }
+          }, 100);
+        });
+      }
+      return async function () {
+        await waitUntilImageEditorIsUnlocked(imageEditor);
+        return cached_function.apply(this, arguments);
+      };
+    })();
+    imageEditor
+      .loadImageFromURL(
+        imgUrl,
+        "SampleImage"
+      )
+      .then((result) => {
+        console.log("🚀 ~ file: imageEditor.js ~ line 125 ~ .then ~ result", result)
+        imageEditor.ui.resizeEditor({
+          imageSize: {
+            oldWidth: result.oldWidth,
+            oldHeight: result.oldHeight,
+            newWidth: result.newWidth,
+            newHeight: result.newHeight,
+          },
+        });
+      })
+      .catch((err) => {
+        console.error("Something went wrong:", err);
+      });
+  }
+
+  const handleClick = () => {
+    setSelectedColor({
+      ...selectedColor,
+      displayColorPicker: !selectedColor.displayColorPicker,
+    });
+  };
+
+  const handleClose = () => {
+    setSelectedColor({ ...selectedColor, displayColorPicker: false });
+  };
+
+  const handleChange = (color) => {
+    let imageEditor = canvasRef.current.getInstance();
+
+    imageEditor.stopDrawingMode();
+    imageEditor.startDrawingMode("FREE_DRAWING", {
+      width: colorWidth,
+      color: color.hex,
+    });
+
+    setSelectedColor({ ...selectedColor, color: color.rgb, start: true });
+  };
+
+  useEffect(() => {
+    $(
+      ".tui-image-editor-container .tui-image-editor-header-buttons div"
+    ).replaceWith(
+      '<button class="tui-image-editor-loader-btn" style="background-color: #fff;border: 1px solid #ddd;color: #222;font-family: NotoSans, sans-serif;font-size: 12px" >Recarregar</button>'
+    );
+
+    $(
+      ".tui-image-editor-header-buttons .tui-image-editor-download-btn"
+    ).remove();
+
+    document
+      .querySelector(
+        ".tui-image-editor-header-buttons .tui-image-editor-loader-btn"
+      )
+      ?.addEventListener("click", LoadImage);
+
+    LoadImage;
+  }, [canvasRef]);
+
+  useEffect(() => {
+    if (canvasRef.current.getInstance()._invoker._isLocked) {
+      LoadImage();
+    }
+  }, [canvasRef]);
+
+  const styles = reactCSS({
+    default: {
+      color: {
+        width: "36px",
+        height: "14px",
+        borderRadius: "2px",
+        background: `rgba(${selectedColor.color.r}, ${selectedColor.color.g}, ${selectedColor.color.b}, ${selectedColor.color.a})`,
+      },
+      swatch: {
+        padding: "5px",
+        background: "#fff",
+        borderRadius: "1px",
+        boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+        display: "inline-block",
+        cursor: "pointer",
+      },
+      popover: {
+        position: "absolute",
+        zIndex: "2",
+      },
+      cover: {
+        position: "fixed",
+        top: "0px",
+        right: "0px",
+        bottom: "0px",
+        left: "0px",
+      },
+    },
+  });
+
   return (
     <>
-    <ImageEditor
-      ref={canvasRef}
-      includeUI={{
-        loadImage: {
-          path: url,
-          name: "SampleImage",
-        },
-        theme,
-        menu: [
-          "shape",
-          "filter",
-          "text",
-          "mask",
-          "icon",
-          "draw",
-          "crop",
-          "flip",
-          "rotate",
-        ],
-        initMenu: "filter",
-        uiSize: {
-          width: "80%",
-          height: "100%",
-        },
-        menuBarPosition: "bottom",
-      }}
-      cssMaxHeight={700}
-      cssMaxWidth={1300}
-      selectionStyle={{
-        cornerSize: 20,
-        rotatingPointOffset: 70,
-      }}
-      usageStatistics={true}
-    />
-    <button
-            onClick={() => {
-              let instance = canvasRef.current.getInstance()
-              console.log(instance)
-              console.log(instance.toDataURL());
-              alert("DataURL written to console")
-            }}
-          >
-            GetDataURL
-          </button></>
-    
+      <ButtonsMenuContainer>
+        <ColorPickerContainer>
+          <div>
+            <div style={styles.swatch} onClick={handleClick}>
+              <div style={styles.color} />
+            </div>
+            {selectedColor.displayColorPicker ? (
+              <div style={styles.popover}>
+                <div style={styles.cover} onClick={handleClose} />
+                <SketchPicker
+                  color={selectedColor.color}
+                  presetColors={presetColors}
+                  disableAlpha
+                  onChange={handleChange}
+                />
+              </div>
+            ) : null}
+          </div>
+          {!selectedColor?.start && (
+            <ColorSelectedText>
+              Selecione uma cor para começar
+            </ColorSelectedText>
+          )}
+        </ColorPickerContainer>
+        <Submit onClick={(e) => handleSubmit(e)}>Salvar</Submit>
+      </ButtonsMenuContainer>
+
+      <LinearProgressContainer>
+        <LinearProgressWithLabel
+          variant="determinate"
+          value={progresspercent}
+        />
+      </LinearProgressContainer>
+
+      <ImageEditor
+        ref={canvasRef}
+        includeUI={{
+          loadImage: {
+            path: "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+            name: "Blank",
+          },
+          theme,
+          menu: [
+            "shape",
+            "filter",
+            "text",
+            "mask",
+            "icon",
+            "crop",
+            "flip",
+            "rotate",
+          ],
+          uiSize: {
+            width: "80%",
+            height: "100%",
+          },
+          menuBarPosition: "bottom",
+        }}
+        cssMaxHeight={700}
+        cssMaxWidth={1300}
+        selectionStyle={{
+          cornerSize: 20,
+          rotatingPointOffset: 70,
+        }}
+        usageStatistics={true}
+      />
+    </>
   );
-};
+}
 
 export default CustomImageEditor;
