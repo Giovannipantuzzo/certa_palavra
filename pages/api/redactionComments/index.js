f0b051dcca55ff497ab15b7999053c063084a36a
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2022-07-06 18:33:19 -0300

    Auth

--- pages/api/redactionComments/index.js
+++ pages/api/redactionComments/index.js
@@ -1,14 +1,14 @@
 import { createComment, getAllComments } from '../../../src/controllers/RedactionController';
-// import { isAdmin } from '../../../src/utils/Auth';
+import { withAuthValidation } from '../../../src/utils/Auth';
 
 export default function handler(req, res) {
   try {
     const { method } = req;
     if (method === 'POST') {
-      return createComment(req, res);
+      return withAuthValidation(createComment)(req, res);
     }
     if (method === 'GET') {
-      return getAllComments(req, res);
+      return withAuthValidation(getAllComments)(req, res);
     }
     return res.status(500).json({ message: 'Internal Server Error' });
   } catch (err) {
