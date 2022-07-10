c0c69e9e116af093b1a0bfb0ee17e160b50a826c
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2022-07-10 17:27:45 -0300

    base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://certa-palavra.vercel.app/api' : 'http://localhost:3000/api',
+  baseURL: 'https://certa-palavra.vercel.app/api',
 });
 
 export default api;
