a31e3e47698fc175e8e6f01c186acc128bfa30a3
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2022-07-10 17:39:52 -0300

    reading client

--- knexfile.js
+++ knexfile.js
@@ -29,4 +29,31 @@ module.exports = {
       max: 10,
     },
   },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    seeds: {
+      directory: './src/database/seeds',
+    },
+    useNullAsDefault: true,
+    pool: {
+      // afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+      min: 0,
+      max: 10,
+    },
+  },
 };
